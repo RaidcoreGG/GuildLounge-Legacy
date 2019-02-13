853bba851297b1cb4ede2760337e89ec3df4b642
DeltaxHunter <46466697+DeltaxHunter@users.noreply.github.com>
2019-02-13 06:07:31 +0100

    added ApiKeyInfo panel

---
+++ Controls/ApiKeyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Forms;
+
+namespace GuildLounge.Controls
+{
+    public partial class ApiKeyInfo : UserControl
+    {
+        public ApiKeyInfo()
+        {
+            InitializeComponent();
+        }
+
+        public void UpdatePermissions(string permissions)
+        {
+            panelAccount.Allowed = permissions.Contains("account");
+            panelInventories.Allowed = permissions.Contains("inventories");
+            panelCharacters.Allowed = permissions.Contains("characters");
+            panelTradingPost.Allowed = permissions.Contains("tradingpost");
+            panelWallet.Allowed = permissions.Contains("wallet");
+
+            panelUnlocks.Allowed = permissions.Contains("unlocks");
+            panelPvP.Allowed = permissions.Contains("pvp");
+            panelBuilds.Allowed = permissions.Contains("builds");
+            panelProgression.Allowed = permissions.Contains("progression");
+            panelGuilds.Allowed = permissions.Contains("guilds");
+        }
+    }
+}
---
+++ Controls/PermissionPanel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuildLounge.Controls
+{
+    public partial class PermissionPanel : Control
+    {
+        private bool m_bAllowed { get; set; }
+        public bool Allowed
+        {
+            get
+            {
+                return m_bAllowed;
+            }
+            set
+            {
+                m_bAllowed = value;
+                if (m_bAllowed)
+                    m_oColor = Color.Green;
+                else
+                    m_oColor = Color.Red;
+                Refresh();
+            }
+        }
+        private Color m_oColor { get; set; }
+
+        public PermissionPanel()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            pe.Graphics.FillRectangle(new SolidBrush(m_oColor), 0, 0, Width, Height);
+        }
+    }
+}
