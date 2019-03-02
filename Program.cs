b8fbb3db9ef757a07c0cd9ac902f161b07d67b3e
DeltaxHunter <46466697+DeltaxHunter@users.noreply.github.com>
2019-03-02 14:12:48 +0100

    implemented mutex

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace GuildLounge
 {
@@ -12,12 +14,31 @@ namespace GuildLounge
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        static Mutex mutex = new Mutex(true, "{445d729e-f4d5-42cd-b7a9-c094b88e6d98}");
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            if(mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Main());
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                NativeMethods.PostMessage((IntPtr)NativeMethods.HWND_BROADCAST, NativeMethods.WM_SHOWME, IntPtr.Zero, IntPtr.Zero);
+            }
         }
+    }
+
+    internal class NativeMethods
+    {
+        public const int HWND_BROADCAST = 0xffff;
+        public static readonly int WM_SHOWME = RegisterWindowMessage("WM_SHOWME");
+        [DllImport("user32")]
+        public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
+        [DllImport("user32")]
+        public static extern int RegisterWindowMessage(string message);
     }
 }
