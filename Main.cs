0baecaad74717e763e50087bd3b3f8ba63f2121e
DeltaxHunter <46466697+DeltaxHunter@users.noreply.github.com>
2019-03-16 03:51:29 +0100

    removed mutex entirely

--- Main.cs
+++ Main.cs
@@ -87,29 +87,6 @@ namespace GuildLounge
         }
 
         #region misc
-        //Required for mutex
-        protected override void WndProc(ref Message m)
-        {
-            if (m.Msg == NativeMethods.WM_SHOWME)
-            {
-                ShowMe();
-            }
-            base.WndProc(ref m);
-        }
-        private void ShowMe()
-        {
-            if (WindowState == FormWindowState.Minimized)
-            {
-                WindowState = FormWindowState.Normal;
-            }
-            // get our current "TopMost" value (ours will always be false though)
-            bool top = TopMost;
-            // make our form jump to the top of everything
-            TopMost = true;
-            // set it back to whatever it was
-            TopMost = top;
-        }
-
         private void InitializeSettings()
         {
             //Upgrade the settings if necessary and save them
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 using System.Drawing;
 using System.Threading;
 using System.Runtime.InteropServices;
@@ -14,31 +15,22 @@ namespace GuildLounge
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static Mutex mutex = new Mutex(true, "{445d729e-f4d5-42cd-b7a9-c094b88e6d98}");
         [STAThread]
         static void Main()
         {
-            if(mutex.WaitOne(TimeSpan.Zero, true))
+            Process[] running = Process.GetProcessesByName("GuildLounge");
+            if(running.Length > 1)
             {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Main());
-                mutex.ReleaseMutex();
+                SetForegroundWindow(running[running.Length - 1].MainWindowHandle);
+                Environment.Exit(0);
             }
-            else
-            {
-                NativeMethods.PostMessage((IntPtr)NativeMethods.HWND_BROADCAST, NativeMethods.WM_SHOWME, IntPtr.Zero, IntPtr.Zero);
-            }
-        }
-    }
 
-    internal class NativeMethods
-    {
-        public const int HWND_BROADCAST = 0xffff;
-        public static readonly int WM_SHOWME = RegisterWindowMessage("WM_SHOWME");
-        [DllImport("user32")]
-        public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
-        [DllImport("user32")]
-        public static extern int RegisterWindowMessage(string message);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
     }
 }
